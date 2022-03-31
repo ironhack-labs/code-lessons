29bb422554f39aa7fc8b427801869cf3a2699581
Aleksandra Bošković <sandraboskovic@hotmail.com>
2022-04-01 00:07:18 +0200

    Update route-guard.config.js

--- basicAuth/configs/route-guard.config.js
+++ basicAuth/configs/route-guard.config.js
@@ -1,16 +1,25 @@
 // configs/route-guard.config.js
 
-// Auth middleware function
-const isLoggedIn = (req, res, next) => {
-  // If request has cookie with session id, allow access - next ()
-  if (req.session.currentUser) {
-    next();
-  } else {
-    // Otherwise, redirect to login
-    res.redirect('/login');
-  }
-};
+// Auth middleware functions
 
-module.exports = {
-  isLoggedIn,
-};
+// checks if the user is logged in when trying to access a specific page
+const isLoggedIn = (req, res, next) => {
+    if (!req.session.currentUser) {
+      return res.redirect('/login');
+    }
+    next();
+  };
+   
+  // if an already logged in user tries to access the login page it
+  // redirects the user to the home page
+  const isLoggedOut = (req, res, next) => {
+    if (req.session.currentUser) {
+      return res.redirect('/');
+    }
+    next();
+  };
+   
+  module.exports = {
+    isLoggedIn,
+    isLoggedOut
+  };
